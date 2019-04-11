10a4fcc878704115a603f6f4f9b6ec5ef6c20942
FFY00 <filipe.lains@gmail.com>
2019-04-11 20:47:27 +0100

    polybar: make the network script wait one cycle before showing data

--- .config/polybar/network-traffic.sh
+++ .config/polybar/network-traffic.sh
@@ -40,14 +40,19 @@ update_interfaces() {
 old_up=-1
 old_down=-1
 last_update=$UPDATE_INTERFACES
+skip=0
 while true; do
     down=0
     up=0
 
     ((last_update++))
     if [ "$last_update" -ge "$UPDATE_INTERFACES" ]; then
+        old=${INTERFACES[@]}
+
         update_interfaces
         last_update=0
+
+        [[ "${old[@]}" != "${INTERFACES[@]}" ]] && skip=1 || skip=0
     fi
 
     if [ ${#INTERFACES[@]} -gt 0 ]; then
@@ -56,7 +61,9 @@ while true; do
             up=$(expr $up + $(cat /sys/class/net/"$interface"/statistics/tx_bytes))
         done
 
-        echo "$(join_by ', ' "${INTERFACES[@]}") %{T11}🠉%{T-} $(print_bytes $(average $old_up $up)) %{T11}🠋%{T-} $(print_bytes $(average $old_down $down))"
+        if [ $skip -eq 0 ]; then
+            echo "$(join_by ', ' "${INTERFACES[@]}") %{T11}🠉%{T-} $(print_bytes $(average $old_up $up)) %{T11}🠋%{T-} $(print_bytes $(average $old_down $down))"
+        fi
      else
         echo
      fi
