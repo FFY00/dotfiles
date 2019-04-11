350fe4cab8f113be8b1a55676733041ff793e208
FFY00 <filipe.lains@gmail.com>
2019-04-11 20:52:37 +0100

    fish: add gg function and ident pp

--- .config/fish/functions/pp.fish
+++ .config/fish/functions/pp.fish
@@ -1,6 +1,7 @@
+# Defined in /tmp/fish.uLaQpX/pp.fish @ line 2
 function pp
 	set -l package_path (find ~/git/arch/community/ -mindepth 1 -maxdepth 1 -type d -not -name '.*' -exec basename '{}' \; | fzf | xargs -r -I '{}' echo ~/git/arch/community/'{}'/trunk)
-if ! test -z $package_path
-cd "$package_path"
-end
+	if ! test -z $package_path
+		cd "$package_path"
+	end
 end
