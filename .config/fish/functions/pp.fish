100482f9eda881de767264aab236dae923539d61
FFY00 <filipe.lains@gmail.com>
2019-04-11 20:10:19 +0100

    fish: add some personal shortcuts for arch packaging

---
+++ .config/fish/functions/pp.fish
@@ -0,0 +1,6 @@
+function pp
+	set -l package_path (find ~/git/arch/community/ -mindepth 1 -maxdepth 1 -type d -not -name '.*' -exec basename '{}' \; | fzf | xargs -r -I '{}' echo ~/git/arch/community/'{}'/trunk)
+if ! test -z $package_path
+cd "$package_path"
+end
+end
---
+++ .config/fish/functions/pu.fish
@@ -0,0 +1,4 @@
+# Defined in /tmp/fish.NnZCAx/pu.fish @ line 2
+function pu
+	pp ;and rmpt; svn update ..
+end
