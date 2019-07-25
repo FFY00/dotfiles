27b4fd5650131bd3c1fa8040d187cebaac02520a
Filipe Laíns <lains@archlinux.org>
2019-07-25 09:00:10 +0100

    fish: add a protection mechanism to gr and grf
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- .config/fish/functions/gr.fish
+++ .config/fish/functions/gr.fish
@@ -1,3 +1,13 @@
 function gr
+	read -l -n 1 -P 'Do you want to do a git reset? (y/n) ' confirm
+	if test "$confirm" != 'y' -a "$confirm" != 'Y'
+		echo 'Reset aborted.'
+		return
+	end
+	read -l -n 1 -P 'Do you *really* want to do a git reset? (y/n) ' confirm
+	if test "$confirm" != 'y' -a "$confirm" != 'Y'
+		echo 'Reset aborted.'
+		return
+	end
 	command git reset $argv
 end
