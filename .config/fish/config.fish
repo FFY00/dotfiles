1ab3b9062ad46e6c2020af3584e9bbfb3eb6aa22
Filipe Laíns <lains@riseup.net>
2020-12-11 18:29:06 +0000

    fish: use gnome-keyring-agent as a ssh agent
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,7 @@ bash -c 'eval $(dircolors -b ~/.dircolors)'
 set -gx PATH ~/.local/bin $PATH
 set -gx PATH ~/.cargo/bin $PATH
 set -gx PATH ~/.dotnet/tools $PATH
+
+for var in (gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
+    set -xU (echo $var | string split '=')
+end
