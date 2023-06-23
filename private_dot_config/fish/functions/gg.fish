e729ec80a638979b333ec6380c225cda071d8759
Filipe Laíns <lains@riseup.net>
2023-06-23 23:58:25 +0100

    fish: gg: allow arbitrary input
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- private_dot_config/fish/functions/gg.fish
+++ private_dot_config/fish/functions/gg.fish
@@ -3,10 +3,21 @@ function gg
 	set -l gitdir			~/git
 	set -l find_filter_args		-mindepth 1 -maxdepth 1 -type d -not -name '.*'
 	set -l name_transform_cmd	basename {} \;
-	set -l fzf_args			"--preview=ls --color=always $gitdir/{}"
-	# execute
-	set -l project (find $gitdir $find_filter_args -exec $name_transform_cmd | fzf $fzf_args)
+	set -l fzf_args			--preview="ls --color=always $gitdir/{}"
+	# get user input
+	set -l project (
+		begin
+			# add . as an option to allow going to the root
+			echo .
+			# find
+			find $gitdir $find_filter_args -exec $name_transform_cmd
+		# use --print-query (and then tail -n1) to allow arbitrary input
+		end | fzf --print-query $fzf_args | tail -n1
+	)
+	# change directory
 	if ! test -z $project
-		cd $gitdir/$project
+		# cd to $gitdir first to leave the user there if $project doesn't exist
+		cd $gitdir
+		cd $project
 	end
 end
