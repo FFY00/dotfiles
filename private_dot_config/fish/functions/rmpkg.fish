f4390f3305b557db6eec641bdc79a484920ec729
Filipe Laíns <lains@riseup.net>
2023-08-16 01:57:38 +0100

    fish: rmpkg: simplify
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- private_dot_config/fish/functions/rmpkg.fish
+++ private_dot_config/fish/functions/rmpkg.fish
@@ -1,18 +1,11 @@
 function rmpkg
-	set -x folder (basename (pwd))
-	if test $folder = 'trunk'
-		set -x folder (echo $folder | sed "s|/$folder||g" | xargs basename)
-	else if test -d trunk
+	if test -d trunk
 		echo "Changing directory..."
 		cd 'trunk'
-	else
-		return
 	end
 	if test -e 'PKGBUILD'
 		set -lx files *.log *.pkg.* src pkg .SRCINFO
-		for file in $files
-			rm -rf $file
-		end
+		rm -rf $files
 	else
 		echo "Folder doesn't contain a PKGBUILD..."
 	end
