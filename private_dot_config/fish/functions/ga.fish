2bc447d050036493db908ee8c2db160a8a734ede
Filipe Laíns <lains@riseup.net>
2024-04-28 16:35:39 +0100

    fish: ga: fix files being grouped into a directory in certain scenarios
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- private_dot_config/fish/functions/ga.fish
+++ private_dot_config/fish/functions/ga.fish
@@ -1,4 +1,3 @@
-# Defined in /tmp/fish.TnU7Um/ga.fish @ line 2
 function ga
 	set -l git_args -c color.status=always
 	set -l fzf_args --preview='git diff --no-ext-diff --color=always -- {-1} | sed 1,4d; bat --color=always --plain {-1}'
@@ -8,14 +7,16 @@ function ga
 	# [0-9:-?] - match 0x30 to 0x3F
 	# [@-~A-Za-z] - match 0x40 to 0x7F
 	set -l ctrseq '\(\\x1b\[[0-9:-?]*[@-~A-Za-z]\)'
-	set -l files (
-		git $git_args status -s 2>/dev/null | sed "/^$ctrseq*\S$ctrseq*\s/d" \
+	set -l files (git ls-files -m -o --exclude-standard (git rev-parse --show-toplevel))
+	set -l files_to_add (
+		git $git_args status -s $files 2>/dev/null \
+		| sed "/^$ctrseq*\S$ctrseq*\s/d" \
 		| fzf -m --ansi $fzf_args \
 		| sed -e 's/^\s//' -e 's/\s+/ /g' \
 		| cut -d' ' -f2
 	)
-	if test $status -eq 0; and string length -q "$files"
-		echo git add "$files"
-		git add $files
+	if test $status -eq 0; and string length -q "$files_to_add"
+		echo git add "$files_to_add"
+		git add $files_to_add
 	end
 end
