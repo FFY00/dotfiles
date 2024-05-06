91c17f2a1537d04619089253291c1d4a322c37a0
Filipe Laíns <lains@riseup.net>
2024-05-06 22:22:44 +0100

    fish: ga: fix the preview containing both git diff and contents
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- private_dot_config/fish/functions/ga.fish
+++ private_dot_config/fish/functions/ga.fish
@@ -1,6 +1,10 @@
 function ga
+	set -l is_tracked_cmd 'git ls-files --error-unmatch {-1} >&2 2>/dev/null'
+	set -l git_diff_cmd 'git diff --no-ext-diff --color=always -- {-1} | sed 1,4d'
+	set -l cat_cmd 'bat --color=always --plain {-1}'
+
 	set -l git_args -c color.status=always
-	set -l fzf_args --preview='git diff --no-ext-diff --color=always -- {-1} | sed 1,4d; bat --color=always --plain {-1}'
+	set -l fzf_args --preview="$is_tracked_cmd && $git_diff_cmd || $cat_cmd"
 	# See https://en.wikipedia.org/wiki/ANSI_escape_code#Description
 	# Control sequence format: ESC [ (parameter bytes - 0x30-0x3F)* (final byte - 0x40-0x7F)
 	# \\x1b - match ESC
