e2bec3252ec2860c29eec2d22775717b220a7468
Filipe Laíns <lains@riseup.net>
2024-05-28 14:47:47 +0100

    fish: gg: use fzf-cd helper
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- private_dot_config/fish/functions/gg.fish
+++ private_dot_config/fish/functions/gg.fish
@@ -1,23 +1,4 @@
 function gg
-	# config
-	set gitdir			~/git
-	set find_filter_args		-mindepth 1 -maxdepth 1 -type d -not -name '.*'
-	set name_transform_cmd	basename {} \;
-	set fzf_args			--preview="ls --color=always $gitdir/{}"
-	# get user input
-	set project (
-		begin
-			# add . as an option to allow going to the root
-			echo .
-			# find
-			find $gitdir $find_filter_args -exec $name_transform_cmd
-		# use --print-query (and then tail -n1) to allow arbitrary input
-		end | fzf --print-query $fzf_args | tail -n1
-	)
-	# change directory
-	if ! test -z $project
-		# cd to $gitdir first to leave the user there if $project doesn't exist
-		cd $gitdir
-		cd $project
-	end
+	fzf-cd ~/git
+	return $status
 end
