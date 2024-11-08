a8cdf758d70daee804fe601829d3af43a6151a83
Filipe Laíns <lains@riseup.net>
2024-11-08 21:03:28 +0000

    fish: has-commit: uncapitalize usage prefix
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- private_dot_config/fish/functions/has-commit.fish
+++ private_dot_config/fish/functions/has-commit.fish
@@ -1,6 +1,6 @@
 function has-commit --argument commit
 	if test -z $commit
-		echo-color red "Usage: $_ <commit> [num]" >&2
+		echo-color red "usage: $_ <commit> [num]" >&2
 		return 1
 	end
 	command git rev-parse $commit &>/dev/null
