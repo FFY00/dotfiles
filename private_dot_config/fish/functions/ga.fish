0c9b96390dc63fa8475e97ed297d7a171936ae01
Filipe Laíns <lains@riseup.net>
2024-11-08 23:39:52 +0000

    fish: ga: fix files with spaces in name
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- private_dot_config/fish/functions/ga.fish
+++ private_dot_config/fish/functions/ga.fish
@@ -1,7 +1,8 @@
 function ga
-	set is_tracked_cmd 'git ls-files --error-unmatch {-1} >&2 2>/dev/null'
-	set git_diff_cmd 'git diff --no-ext-diff --color=always -- {-1} | sed 1,4d'
-	set cat_cmd 'bat --color=always --plain {-1}'
+	set file '"$(echo {} | sed "s/^.. //")"'  # Use this instead of the placeholder, as we need to remove the git status prefix
+	set is_tracked_cmd "git ls-files --error-unmatch $file >&2 2>/dev/null"
+	set git_diff_cmd "git diff --no-ext-diff --color=always -- $file | sed 1,4d"
+	set cat_cmd "bat --color=always --plain $file"
 
 	set git_args -c color.status=always
 	set fzf_args --preview="$is_tracked_cmd && $git_diff_cmd || $cat_cmd"
@@ -14,10 +15,9 @@ function ga
 	set files (git ls-files -m -o --exclude-standard (git rev-parse --show-toplevel))
 	set files_to_add (
 		git $git_args status -s $files 2>/dev/null \
-		| sed "/^$ctrseq*\S$ctrseq*\s/d" \
+		| sed -e "/^$ctrseq*\S$ctrseq*\s/d" -e 's/"//' -e 's/"$//' \
 		| fzf -m --ansi $fzf_args \
-		| sed -e 's/^\s//' -e 's/\s+/ /g' \
-		| cut -d' ' -f2
+		| sed -e 's/^\s//' -e 's/\s+/ /g' -e 's/^.*.* //'
 	)
 	if test $status -eq 0; and string length -q "$files_to_add"
 		run-command git add $files_to_add
