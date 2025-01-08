aa4184341d1014cc232a21b63d5995c88de49253
Filipe Laíns <lains@riseup.net>
2025-01-08 18:56:43 +0000

    fish: fix ccache on macOS
    Signed-off-by: Filipe Laíns <lains@riseup.net

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -15,10 +15,33 @@ set -ax PATH ~/.cabal/bin
 set -ax PATH ~/.dotnet/tools
 set -ax PATH ~/go/bin
 
+# OS-specific
+switch (uname)
+    case Linux
+        # Configure GCR as the ssh agent
+        set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
+        # Use gnome-keyring-daemon for the keyring
+        gnome-keyring-daemon --start --components=keyring,pkcs11,secrets | eval-shell-vars -U
+        # Platform configuration
+        set CCACHE_BINDIR /usr/lib/ccache/bin
+
+    case Darwin
+        # Start ssh-agent
+        ssh-agent -c | eval-shell-vars --shell=csh
+        # Homebrew paths
+        set -ax PATH /opt/homebrew/opt/python@3.11/libexec/bin
+        set -ax PATH /opt/homebrew/opt/coreutils/libexec/gnubin
+        set -ax PATH /opt/homebrew/bin
+        # Platform configuration
+        set CCACHE_BINDIR /opt/homebrew/opt/ccache/libexec
+end
+
 # Set default programs
 ## Compilers
-set -x CC /usr/lib/ccache/bin/cc
-set -x CXX /usr/lib/ccache/bin/c++
+if set -q CCACHE_BINDIR
+    set -x CC $CCACHE_BINDIR/cc
+    set -x CXX $CCACHE_BINDIR/c++
+end
 ## General
 set -x EDITOR nvim
 set -x VISUAL nvim
@@ -39,20 +62,3 @@ set -x QT_AUTO_SCREEN_SCALE_FACTOR 0
 
 # Set dynamic variables
 dircolors -c ~/.dircolors | eval-shell-vars --shell=csh
-
-# OS-specific
-switch (uname)
-    case Linux
-        # Configure GCR as the ssh agent
-        set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
-        # Use gnome-keyring-daemon for the keyring
-        gnome-keyring-daemon --start --components=keyring,pkcs11,secrets | eval-shell-vars -U
-
-    case Darwin
-        # Start ssh-agent
-        ssh-agent -c | eval-shell-vars --shell=csh
-        # Homebrew paths
-        set -ax PATH /opt/homebrew/opt/python@3.11/libexec/bin
-        set -ax PATH /opt/homebrew/opt/coreutils/libexec/gnubin
-        set -ax PATH /opt/homebrew/bin
-end
