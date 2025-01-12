1e32e1a7bcd40704315a3664c98bc6fffcf005ab
Filipe Laíns <lains@riseup.net>
2025-01-12 17:07:31 +0000

    fish: run-command: escape command in the log message
    Signed-off-by: Filipe Laíns <lains@riseup.net

--- private_dot_config/fish/functions/run-command.fish
+++ private_dot_config/fish/functions/run-command.fish
@@ -1,5 +1,15 @@
 function run-command
-	echo-color dim "\$ $argv"
+	# Build a shell-escaped version of the command
+	for arg in $argv
+		switch "$arg"
+			case '* *'
+				set -a repr "'$arg'"
+			case '*'
+				set -a repr "$arg"
+		end
+	end
+	# Print and execute command
+	echo-color dim "\$ $repr"
 	command $argv
 	return $status
 end
