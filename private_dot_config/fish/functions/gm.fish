760fff3acb129bf4d365175c90898541c202a950
Filipe Laíns <lains@riseup.net>
2025-03-30 09:04:44 +0100

    fish: gm: allow passing non-message options
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- private_dot_config/fish/functions/gm.fish
+++ private_dot_config/fish/functions/gm.fish
@@ -1,8 +1,11 @@
 # Defined in /tmp/fish.dkUCBe/gm.fish @ line 2
-function gm
-	set options
-	for i in (seq 1 (count $argv))
-		set -x options $options -m $argv[$i]
+function gm --wraps 'git commit'
+	for arg in $argv
+		# If it isn't an option (starts with -), add -m/--message.
+		if not string match -- '-*' "$arg"
+			set -a options -m
+		end
+		set -a options "$arg"
 	end
 	command git commit $options
 end
