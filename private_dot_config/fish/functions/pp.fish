369c3a49612e94f4bef197d69a1145089da504dc
Filipe Laíns <lains@riseup.net>
2025-04-16 21:58:54 +0100

    fish: pp: use fzf-cd
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- private_dot_config/fish/functions/pp.fish
+++ private_dot_config/fish/functions/pp.fish
@@ -1,8 +1,5 @@
 # Defined in /tmp/fish.uLaQpX/pp.fish @ line 2
 function pp
-	set package (find ~/git/arch/community/ -mindepth 1 -maxdepth 1 -type d -not -name '.*' -exec basename '{}' \; 2> /dev/null | fzf)
-	set package_path ~/git/arch/community/"$package"/trunk
-	if test -d $package_path
-		cd "$package_path"
-	end
+	fzf-cd ~/git/arch
+	return $status
 end
