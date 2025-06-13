c63a87b85db5217097ca5aa770b34a213c453217
Filipe Laíns <lains@riseup.net>
2025-06-13 16:48:23 +0100

    fish: gpg-secret: fix keygrip, which was meant to be short 🙃
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- private_dot_config/fish/functions/gpg-secret.fish
+++ private_dot_config/fish/functions/gpg-secret.fish
@@ -1,4 +1,3 @@
-function gpg-secret --wraps='gpg --with-keygrip --with-fingerprint --keyid-format=short -K' --description 'alias gpg-secret=gpg --with-keygrip --with-fingerprint --keyid-format=short -K'
-  gpg --with-keygrip --with-fingerprint --keyid-format=short -K $argv
-        
+function gpg-secret --wraps='gpg --with-keygrip --with-fingerprint --keyid-format=short -K' --description 'alias gpg-secret=gpg --with-keygrip --with-fingerprint --keyid-format=long -K'
+  gpg --with-keygrip --with-fingerprint --keyid-format=long -K $argv
 end
