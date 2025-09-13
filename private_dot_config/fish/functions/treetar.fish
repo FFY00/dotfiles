8aac5f3ae3650bcec23a4009c6625623a786a38d
Filipe Laíns <lains@riseup.net>
2025-09-13 02:54:20 +0100

    fish: treetar: search for package in the current directory if none was passed
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- private_dot_config/fish/functions/treetar.fish
+++ private_dot_config/fish/functions/treetar.fish
@@ -1,4 +1,7 @@
 function treetar
+	# Search for arch package in current directory if no file was passed
+	test -z "$argv" && set -a argv (findpkg)
+
 	for entry in $argv
 		command tar -tf $entry 2> /dev/null | treeify
 	end
