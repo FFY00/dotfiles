c54783818069b215435e9dac1b3bb782d5f83a34
Filipe Laíns <lains@riseup.net>
2026-04-10 14:41:06 +0100

    fish: echo-color: add support for underline styles
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- private_dot_config/fish/functions/echo-color.fish
+++ private_dot_config/fish/functions/echo-color.fish
@@ -1,5 +1,6 @@
 function echo-color
 	set styles bold dim italics reverse underline
+	set underline_styles usingle udouble ucurly udotted udashed
 
 	set style_string $argv[1]
 	set echo_args $argv[2..]
@@ -7,10 +8,13 @@ function echo-color
 	# Build set_color_args from the style string.
 	# Eg.:
 	# 	dim+bold+red > '--dim --bold red'
+	# 	dim+bold+ucurly+red > '--dim --bold -ucurly red'
 	# 	dim+bold > '--dim --bold'
 	for entry in (string split + $style_string)
 		if contains $entry $styles
 			set -a set_color_args "--$entry"
+		else if contains $entry $underline_styles
+			set -a set_color_args "-$entry"
 		else
 			set -a set_color_args $entry
 		end
